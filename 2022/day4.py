38e40ffeded2e0f8ceb54e4ca63ae00ac854a3b3
Andrew Dixon <andrewdixon_97@hotmail.co.uk>
2022-12-05 20:37:29 +0000

    d4p1

---
+++ 2022/day4.py
@@ -0,0 +1,32 @@
+with open('2022/data/day4.txt') as f:
+    data = [s.strip().split(',') for s in f.readlines()]
+
+class Elf():
+    def __init__(self, elf):
+        self.start = int(elf[0])
+        self.end = int(elf[1])
+
+    def __repr__(self):
+        return ' '*self.start + str(self.start) + '-'*(self.end-self.start) + str(self.end) + ' '*5
+
+
+class Pair():
+    def __init__(self, elf1, elf2):
+        self.elf1 = Elf(elf1)
+        self.elf2 = Elf(elf2)
+        self.fully_contained = None
+
+    def __repr__(self):
+        output = self.elf1.__repr__() + '\n' + self.elf2.__repr__() + str(self.fully_contained) + '\n\n'
+        return output
+
+
+pairs = [Pair(d[0].split('-'), d[1].split('-')) for d in data]
+
+for pair in pairs:
+    big_top = (pair.elf1.start <= pair.elf2.start) and (pair.elf1.end >= pair.elf2.end)
+    big_bottom = (pair.elf1.start >= pair.elf2.start) and (pair.elf1.end <= pair.elf2.end)
+    pair.fully_contained = big_top or big_bottom
+
+contained_count = sum([pair.fully_contained for pair in pairs])
+print('part1:', contained_count)
