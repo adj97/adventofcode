6829f939a5d7bb6ba4627b112d87b9df2d1ebac1
Andrew Dixon <andrew.dixon@principleone.co.uk>
2022-12-09 12:43:42 +0000

    d8p2_test

--- 2022/day8.py
+++ 2022/day8.py
@@ -1,4 +1,4 @@
-with open('2022/data/day8.txt') as f:
+with open('2022/data/day8_test.txt') as f:
     raw_input = [line.strip() for line in f.readlines()]
 
 def printsummary(array):
@@ -10,10 +10,17 @@ def printsummary(array):
     for i in range(3):
         print(array[i-3][:3], '...', array[i-3][-3:])
 
+def multiplylist(list):
+    prod = 1
+    for obj in list:
+        prod = prod * obj
+    return prod
+
 class Tree:
     def __init__(self, height):
         self.height = int(height)
         self.isvisible = False
+        self.scenicscore = 0
 
     def __repr__(self):
         return str(1 if self.isvisible else 0)
@@ -48,10 +55,24 @@ for r, row in enumerate(treearray):
                 # trees are visible if max height in ONE of 4 directions
                 # left
                 left = treearray[r][:c]
+                left.reverse()
                 right = treearray[r][c+1:]
                 above = [tree for tree in [treearray[i][c] for i in range(r)]]
+                above.reverse()
                 below = [tree for tree in [treearray[i][c] for i in range(r+1, gridsize)]]
-                max_out = [max([out.height for out in direction]) for direction in [left,right,above,below]]
+                directions = [above, left,right, below]
+                # print(tree.height, [[tree.height for tree in direction] for direction in directions])
+                viewing_distances = []
+                for direction in directions:
+                    visibles = [comparetree.height < tree.height for comparetree in direction]
+                    if False not in visibles:
+                        viewing_distances.append(len(visibles))
+                    else:
+                        viewing_distances.append(visibles.index(False)+1)
+                    #print(visibles, viewing_distances[-1])
+                #print(r,c,tree.height,multiplylist(viewing_distances))
+                tree.scenicscore = multiplylist(viewing_distances)
+                max_out = [max([out.height for out in direction]) for direction in directions]
                 tree.isvisible = any([tree.height > max for max in max_out])
 
 #print(treearray[1])
@@ -63,5 +84,11 @@ for row in treearray:
 visiblecount = sum([sum([tree.isvisible for tree in row]) for row in treearray])
 print('part1 ans:')
 print(visiblecount)
+
+print()
+
+max_score = max([max([tree.scenicscore for tree in row]) for row in treearray])
+print('part2 ans:')
+print(max_score)
 
 
