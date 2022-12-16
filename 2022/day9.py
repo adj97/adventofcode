720ab8c7207e76d5d9a0b25c6069d186867c5677
Andrew Dixon <andrew.dixon@principleone.co.uk>
2022-12-16 23:10:22 +0000

    d9p1

--- 2022/day9.py
+++ 2022/day9.py
@@ -19,6 +19,11 @@ class Knot:
 
         self.history.append([self.x, self.y])
 
+    def move_vec(self, v):
+        self.x += v[0]
+        self.y += v[1]
+        self.history.append([self.x, self.y])
+
     def __repr__(self):
         return f"({self.x}, {self.y})"
 
@@ -28,11 +33,29 @@ def displacement(head: Knot, tail: Knot):
 def norm(vector):
     return sqrt(vector[0]**2+vector[1]**2)
 
+def calculate_tail_reaction(delta):
+    if delta in [[-2,1],[-2,2],[-1,2]]:
+        return [-1, 1]
+    elif delta == [0,2]:
+        return [0,1]
+    elif delta in [[1,2],[2,2],[2,1]]:
+        return [1,1]
+    elif delta == [2,0]:
+        return [1,0]
+    elif delta in [[2,-1],[2,-2],[1,-2]]:
+        return [1,-1]
+    elif delta == [0,-2]:
+        return [0,-1]
+    elif delta in [[-1,-2],[-2,-2],[-2,-1]]:
+        return [-1,-1]
+    elif delta == [-2,0]:
+        return [-1,0]
+
 head = Knot()
 tail = Knot()
 
 moves = []
-with open('2022/data/day9_.txt') as f:
+with open('2022/data/day9.txt') as f:
     for line in f.readlines():
         direction, distance = line.strip().split(' ')
         moves.append([direction, int(distance)])
@@ -41,10 +64,12 @@ for move in moves:
     for i in range(move[1]):
         head.move(move[0])
         d = displacement(head,tail)
-        print(head, tail, norm(d))
         if norm(d)>sqrt(2):
-            print('tail moves')
-        break
-    break
+            tail.move_vec(calculate_tail_reaction(d))
 
-# print(head.history)
+unique_history = []
+for pos in tail.history:
+    if pos not in unique_history:
+        unique_history.append(pos)
+
+print(len(unique_history))
